cc6d2db05719345fd540fa3a5430c34cc7c5ea18
Gurpreet Dhillon <gurpreetdhillon@hotmail.com>
2015-05-23 15:32:38 -0400

    Revert "Revert "Revert "Completed activity_main.xml"""
    This reverts commit 43364d071e9155b348cf44e7009eb699638368da.

--- app/src/main/java/com/junaid/rockpapergame/MainActivity.java
+++ app/src/main/java/com/junaid/rockpapergame/MainActivity.java
@@ -1,25 +1,17 @@
 package com.junaid.rockpapergame;
 
-import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.EditText;
 
 
 public class MainActivity extends ActionBarActivity {
-
-    EditText nameEntered;
-    Button goButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        nameEntered = (EditText)findViewById(R.id.enter_name);
-        goButton = (Button) findViewById(R.id.go_button);
     }
 
     @Override
